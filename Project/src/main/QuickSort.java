23210467495e5d062d7c4288a87729782d2d8dd4
Yosabhat Songmuang <yosabhat.s@kkumail.com>
2025-12-09 20:16:58 +0700

    Add complete quickSort function to the QuickSort.java

--- Project/src/main/QuickSort.java
+++ Project/src/main/QuickSort.java
@@ -1,5 +1,34 @@
 package main;
 
+import java.util.ArrayList;
+
 public class QuickSort {
+	public static ArrayList<Integer> quickSort(ArrayList<Integer> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+
+        int pivot = list.get(list.size() / 2);
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+        ArrayList<Integer> equal = new ArrayList<>();
+
+        for (int num : list) {
+            if (num < pivot) {
+                left.add(num);
+            } else if (num > pivot) {
+                right.add(num);
+            } else {
+                equal.add(num);
+            }
+        }
+
+        ArrayList<Integer> sorted = new ArrayList<>();
+        sorted.addAll(quickSort(left));
+        sorted.addAll(equal);
+        sorted.addAll(quickSort(right));
+
+        return sorted;
+    }
 
 }
